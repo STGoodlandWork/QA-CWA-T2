f50b4c4af12e74e4180370e0a9875a4219e1eef5
Myles <mbrathwaite@academytrainee.com>
2021-01-18 14:54:30 +0000

    MB: Track side of API checked for bugs and implemented Lombok where necessary such as DTO and Domain also added Autowiring

--- Choonz-Starter-master/src/main/java/com/qa/choonz/service/TrackService.java
+++ Choonz-Starter-master/src/main/java/com/qa/choonz/service/TrackService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.qa.choonz.exception.TrackNotFoundException;
@@ -14,47 +15,53 @@ import com.qa.choonz.rest.dto.TrackDTO;
 @Service
 public class TrackService {
 
-    private TrackRepository repo;
-    private ModelMapper mapper;
+	private TrackRepository repo;
+	private ModelMapper mapper;
 
-    public TrackService(TrackRepository repo, ModelMapper mapper) {
-        super();
-        this.repo = repo;
-        this.mapper = mapper;
-    }
+	@Autowired
+	public TrackService(TrackRepository repo, ModelMapper mapper) {
+		super();
+		this.repo = repo;
+		this.mapper = mapper;
+	}
 
-    private TrackDTO mapToDTO(Track track) {
-        return this.mapper.map(track, TrackDTO.class);
-    }
+	private TrackDTO mapToDTO(Track track) {
+		return this.mapper.map(track, TrackDTO.class);
+	}
 
-    public TrackDTO create(Track track) {
-        Track created = this.repo.save(track);
-        return this.mapToDTO(created);
-    }
+	// Create
+	public TrackDTO create(Track track) {
+		Track created = this.repo.save(track);
+		return this.mapToDTO(created);
+	}
 
-    public List<TrackDTO> read() {
-        return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
-    }
+	// Read All
+	public List<TrackDTO> read() {
+		return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
 
-    public TrackDTO read(long id) {
-        Track found = this.repo.findById(id).orElseThrow(TrackNotFoundException::new);
-        return this.mapToDTO(found);
-    }
+	// Read By ID
+	public TrackDTO read(long id) {
+		Track found = this.repo.findById(id).orElseThrow(TrackNotFoundException::new);
+		return this.mapToDTO(found);
+	}
 
-    public TrackDTO update(Track track, long id) {
-        Track toUpdate = this.repo.findById(id).orElseThrow(TrackNotFoundException::new);
-        toUpdate.setName(track.getName());
-        toUpdate.setAlbum(track.getAlbum());
-        toUpdate.setDuration(track.getDuration());
-        toUpdate.setLyrics(track.getLyrics());
-        toUpdate.setPlaylist(track.getPlaylist());
-        Track updated = this.repo.save(toUpdate);
-        return this.mapToDTO(updated);
-    }
+	// Update
+	public TrackDTO update(Track track, long id) {
+		Track toUpdate = this.repo.findById(id).orElseThrow(TrackNotFoundException::new);
+		toUpdate.setName(track.getName());
+		toUpdate.setAlbum(track.getAlbum());
+		toUpdate.setDuration(track.getDuration());
+		toUpdate.setLyrics(track.getLyrics());
+		toUpdate.setPlaylist(track.getPlaylist());
+		Track updated = this.repo.save(toUpdate);
+		return this.mapToDTO(updated);
+	}
 
-    public boolean delete(long id) {
-        this.repo.deleteById(id);
-        return !this.repo.existsById(id);
-    }
+	// Delete
+	public boolean delete(long id) {
+		this.repo.deleteById(id);
+		return !this.repo.existsById(id);
+	}
 
 }
