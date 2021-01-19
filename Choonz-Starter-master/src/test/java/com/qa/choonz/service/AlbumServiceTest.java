ab09ebaa3c162cc7b8186bad9b1c127740711457
ben-noreika <66423091+noreb001@users.noreply.github.com>
2021-01-19 10:17:05 +0000

    first commit

--- Choonz-Starter-master/src/test/java/com/qa/choonz/service/AlbumServiceTest.java
+++ Choonz-Starter-master/src/test/java/com/qa/choonz/service/AlbumServiceTest.java
@@ -1,5 +1,12 @@
 package com.qa.choonz.service;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
 public class AlbumServiceTest {
+	
+	@Autowired
+	private AlbumService
 
 }
