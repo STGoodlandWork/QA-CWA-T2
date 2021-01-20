c964bb9912b055936400114c6b54c8145604603e
ben-noreika <66423091+noreb001@users.noreply.github.com>
2021-01-20 16:01:51 +0000

    bn: improved search feature

--- Choonz-Starter-master/src/main/java/com/qa/choonz/rest/controller/SearchController.java
+++ Choonz-Starter-master/src/main/java/com/qa/choonz/rest/controller/SearchController.java
@@ -1,26 +1,18 @@
 package com.qa.choonz.rest.controller;
-package com.qa.choonz.rest.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.qa.choonz.persistence.domain.Track;
-import com.qa.choonz.rest.dto.TrackDTO;
+import com.qa.choonz.service.AlbumService;
+import com.qa.choonz.service.ArtistService;
+import com.qa.choonz.service.GenreService;
 import com.qa.choonz.service.PlaylistService;
 import com.qa.choonz.service.TrackService;
 
@@ -30,16 +22,19 @@ public class SearchController {
 
 	private TrackService trackservice;
 	private PlaylistService playlistservice;
-	// private GenreController genrecontroller;
-	// private ArtistController artistcontroller;
-	// private AlbumController albumcontroller;
+	private GenreService genreservice;
+	private ArtistService artistservice;
+	private AlbumService albumservice;
 
 	@GetMapping("/search/{query}")
 	public ResponseEntity<List<Object>> search(@PathVariable String query) {
 
-		List<Object> combined = new ArrayList();
+		List<Object> combined = new ArrayList<Object>();
 		combined.addAll(trackservice.search(query));
 		combined.addAll(playlistservice.search(query));
+		combined.addAll(genreservice.search(query));
+		combined.addAll(artistservice.search(query));
+		combined.addAll(albumservice.search(query));
 
 		return new ResponseEntity<List<Object>>(combined, HttpStatus.OK);
 
