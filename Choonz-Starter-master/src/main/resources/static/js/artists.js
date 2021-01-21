b87c322e32664b03099aedc2ac0e4e4c33abbf3b
Syed <smahian@qa.com>
2021-01-21 12:54:01 +0000

    SM: ReadAll and Search completed for track and artist

--- Choonz-Starter-master/src/main/resources/static/js/artists.js
+++ Choonz-Starter-master/src/main/resources/static/js/artists.js
@@ -10,15 +10,6 @@ readAllArtistsButton.onclick = async () => {
   await readArtist(artistName);
 };
 
-<<<<<<< HEAD
-async function readAllArtists() {
-  let response = await fetch(`http://localhost:8082/artist/read`, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json ",
-    },
-  });
-=======
 console.log("Before async");
 async function readArtist(artistName) {
   console.log("After async");
@@ -31,7 +22,6 @@ async function readArtist(artistName) {
       },
     }
   );
->>>>>>> 6ae019fda95fddf435b749c28984ea050b968042
 
   if (!response.ok) {
     console.log(
@@ -42,14 +32,55 @@ async function readArtist(artistName) {
 
   let data = await response.json();
   console.log(data);
+  data.forEach((artist) => {
+          
+    console.log(artist.name);
+    createArtistCard(artist);
+  })
 
-  let div = document.getElementById("myDiv");
-  let artistResult = `Artist: ${data.name} <br>`;
-  let artists = [];
+}
 
-  for (let data_i of data) {
-    let artist = `${data_i}<br>`;
-    artists.push(artist);
-  }
-  console.log(artists);
+//Read All Function Fetch Request
+
+fetch('http://localhost:8082/artist/read')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+        
+        
+        data.forEach((artist) => {
+          
+          console.log(artist.name);
+          createArtistCard(artist);
+        })
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+function createArtistCard(data){
+
+  let myDiv = document.querySelector("#ArtistDisplay");
+  
+  let tempString = '<div class="card text-white bg-dark mb-3 inlineCard" style="max-width: 18rem;">';
+      tempString +='<div class="card-header">Artist</div>';
+      tempString += '<div class="card-body">';
+      tempString +=  '<h5 class="card-title">' + data.name + '</h5>';
+      tempString += '<button type="button" class="btn btn-warning">Update</button>';
+      tempString += '<button type="button" class="btn btn-danger">Delete</button>'
+      tempString +=' </div>';
+      tempString += '</div>';
+
+  myDiv.innerHTML += tempString;
+
 }
