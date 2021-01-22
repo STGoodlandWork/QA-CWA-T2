2e6f88f1dc6e91241718f0ebdb5edaac5d2d7910
TSammyG <s.goodland@hotmail.co.uk>
2021-01-22 08:55:06 +0000

    SG: Added create functions for artist, genre & playlist

---
+++ Choonz-Starter-master/src/main/resources/static/js/create_artist.js
@@ -0,0 +1,34 @@
+document
+  .querySelector("form.viewRecord")
+  .addEventListener("submit", function (stop) {
+    stop.preventDefault();
+    let formElements = document.querySelector("form.viewRecord").elements;
+    console.log(formElements);
+
+    let name = formElements["name"].value;
+
+    let data = {
+      name: name,
+    };
+    sessionStorage.setItem("name", name);
+    console.log("Data to post", data);
+    sendData(data);
+
+    // postData(noteTitle,noteBody)
+  });
+
+function sendData(data) {
+  fetch("http://localhost:8082/artist/create", {
+    method: "post",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(function (data) {
+      console.log("Request succeeded with JSON response", data);
+    })
+    .catch(function (error) {
+      console.log("Request failed", error);
+    });
+}
