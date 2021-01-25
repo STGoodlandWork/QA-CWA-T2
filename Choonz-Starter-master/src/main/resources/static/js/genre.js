93cec7ab6a371e04fdd6271dc64a24a110dbc9ed
TSammyG <s.goodland@hotmail.co.uk>
2021-01-25 15:58:46 +0000

    SG: Begun automated testing, made updates to site navigation and CRUD fixes

--- Choonz-Starter-master/src/main/resources/static/js/genre.js
+++ Choonz-Starter-master/src/main/resources/static/js/genre.js
@@ -105,12 +105,11 @@ function createCard(data) {
   tempString += '<div class="card-header">Tracks</div>';
   tempString += '<div class="card-body">';
   tempString += '<h5 class="card-title">' + data.name + "</h5>";
+  tempString += '<button type="button" class="btn btn-warning">Update</button>';
   tempString +=
-    '<button type="button" class="btn btn-warning">Warning</button>';
-  tempString +=
-    "<button type = 'button' class='btn btn-danger data-id='" +
+    "<button type = 'button' class='btn btn-danger' data-id='" +
     data.id +
-    "' class='delete' onclick='deletePlaylist(" +
+    "' class='delete' onclick='deleteGenre(" +
     data.id +
     ")'> Delete</button>";
   tempString += " </div>";
