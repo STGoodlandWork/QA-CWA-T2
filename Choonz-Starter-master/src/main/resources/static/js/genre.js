996a2917ce7d77433cf0927a7e07b122e050a60b
TSammyG <s.goodland@hotmail.co.uk>
2021-01-25 17:01:44 +0000

    SG: Create, ReadOne & Delete working on all but Genre. Genre ReadOne is not working

--- Choonz-Starter-master/src/main/resources/static/js/genre.js
+++ Choonz-Starter-master/src/main/resources/static/js/genre.js
@@ -4,37 +4,11 @@ genreNameElement.addEventListener("input", (event) => {
   genreName = event.target.value;
 });
 
-let createGenreButton = document.getElementById("createGenreButton");
 let readAllGenresButton = document.getElementById("searchGenreButton");
-
-createGenreButton.onclick = async () => {
-  await createGenre(genreName);
-};
 
 readAllGenresButton.onclick = async () => {
   await readGenre(genreName);
 };
-
-async function createGenre(genreName) {
-  let response = await fetch(`http://localhost:8082/genre/create`, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json ",
-    },
-    body: JSON.stringify({
-      title: genreName,
-    }),
-  });
-
-  if (!response.ok) {
-    console.log(
-      `Looks like there was a problem. Status Code: ${response.status}`
-    );
-    return;
-  }
-  let div = document.getElementById("myDiv");
-  //div.innerText = `New task has been added!`;
-}
 
 async function readGenre(genreName) {
   let response = await fetch(
@@ -57,15 +31,10 @@ async function readGenre(genreName) {
   let data = await response.json();
   console.log(data);
 
-  let div = document.getElementById("myDiv");
-  let genreResult = `Genre: ${data.name} <br>`;
-  let genres = [];
-
-  for (let data_i of data) {
-    let genre = `Genre: ${data_i}<br>`;
-    genres.push(genre);
-  }
-  console.log(genres);
+  data.forEach((genre) => {
+    console.log(genre.name);
+    createCard(genre);
+  });
 }
 
 // ReadAll for Tracks
