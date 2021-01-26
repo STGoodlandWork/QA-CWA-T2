f93fe2491cbc03203a535aaf0434ac43ae970b42
ben-noreika <66423091+noreb001@users.noreply.github.com>
2021-01-26 17:32:51 +0000

    BN:read one artists complete

--- Choonz-Starter-master/src/main/resources/static/js/view_artist.js
+++ Choonz-Starter-master/src/main/resources/static/js/view_artist.js
@@ -24,6 +24,21 @@ function getData(id){
            
              var name = document.getElementById("ArtistDisplay");
              name.querySelector("p").innerHTML = data.name;
+             trackdata = data.tracks
+             albumdata = data.albums
+
+             let trackTable = document.querySelector("table");
+             let albumTable = document.getElementById("album")
+             
+             TrackCreateTableHead(trackTable);
+             createTableBody(trackTable,trackdata);
+             
+             AlbumCreateTableHead(albumTable)
+             createTableBody(albumTable,albumdata)
+
+
+
+
             
              
     
@@ -33,4 +48,59 @@ function getData(id){
       .catch(function(err) {
         console.log('Fetch Error :-S', err);
       });
+    }
+
+
+    function AlbumCreateTableHead(table){
+        let tableHead= table.createTHead();
+          let row = tableHead.insertRow();
+         
+            
+             let th = document.createElement("th");
+             let text = document.createTextNode("Album Name");
+             th.appendChild(text);
+             row.appendChild(th)
+           
+         }
+
+    function TrackCreateTableHead(table){
+        let tableHead= table.createTHead();
+          let row = tableHead.insertRow();
+         
+            
+             let th = document.createElement("th");
+             let text = document.createTextNode("Title");
+             th.appendChild(text);
+             row.appendChild(th)
+ 
+             let th2 = document.createElement("th");
+             let text2 = document.createTextNode("Duration");
+             th2.appendChild(text2);
+             row.appendChild(th2)
+           
+         }
+
+    function createTableBody(table,commentData){
+        for(let commentRecord of commentData){ 
+          let row = table.insertRow();
+            for(let values in commentRecord){
+              
+                if(values == "title" || values == "duration" || values == "name" ){
+              let cell = row.insertCell();
+                let text = document.createTextNode(commentRecord[values]);
+                cell.appendChild(text);
+              
+            }
+            }
+              let newCell = row.insertCell();
+              let myViewButton = document.createElement("a");
+              let myButtonValue = document.createTextNode("View")
+              myViewButton.className ="btn btn-warning pull-right";
+              myViewButton.href="readOne.html?id="+commentRecord.id;
+              myViewButton.appendChild(myButtonValue);
+              newCell.appendChild(myViewButton)
+             
+         }      
+
+
     }
