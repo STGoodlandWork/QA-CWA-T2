6fc2386a065700090f21549df089f104fef2d31a
TSammyG <s.goodland@hotmail.co.uk>
2021-01-27 15:03:52 +0000

    SG: Added IDs in HTML where necessary to test albums & tracks

--- Choonz-Starter-master/src/main/resources/static/js/tracks.js
+++ Choonz-Starter-master/src/main/resources/static/js/tracks.js
@@ -4,37 +4,11 @@ trackNameElement.addEventListener("input", (event) => {
   trackName = event.target.value;
 });
 
-let createTrackButton = document.getElementById("createTrackButton");
 let searchAllTracksButton = document.getElementById("searchTrackButton");
-
-createTrackButton.onclick = async () => {
-  await createTrack(trackName);
-};
 
 searchAllTracksButton.onclick = async () => {
   await searchTrack(trackName);
 };
-
-async function createTrack(trackName) {
-  let response = await fetch(`http://localhost:8082/track/create`, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json ",
-    },
-    body: JSON.stringify({
-      title: trackName,
-    }),
-  });
-
-  if (!response.ok) {
-    console.log(
-      `Looks like there was a problem. Status Code: ${response.status}`
-    );
-    return;
-  }
-  let div = document.getElementById("myDiv");
-  //div.innerText = `New task has been added!`;
-}
 
 async function searchTrack(trackName) {
   let response = await fetch(
@@ -61,24 +35,6 @@ async function searchTrack(trackName) {
     console.log(track.title);
     createCard(track);
   });
-
-  /*
-  let div = document.getElementById("myDiv");
-  let tracks = [];
-
-    // Examine the text in the response
-    response.json().then(function(data) {
-      console.log(data);
-      
-
-      data.forEach((track) => {
-        
-        console.log(track.name);
-        createCard(track);
-      })
-    });
-  }
-  //console.log(tracks);*/
 }
 
 //Read All Function Fetch Request
