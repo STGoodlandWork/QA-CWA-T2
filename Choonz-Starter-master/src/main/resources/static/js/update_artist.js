a4d1c061dd60a645204e0c0e4f12aea33fee1330
TSammyG <s.goodland@hotmail.co.uk>
2021-01-28 22:48:49 +0000

    SG: HOTFIX: Fixed update artist

--- Choonz-Starter-master/src/main/resources/static/js/update_artist.js
+++ Choonz-Starter-master/src/main/resources/static/js/update_artist.js
@@ -18,38 +18,8 @@ function getData(id) {
       // Examine the text in the response
       response.json().then(function (data) {
         console.log("MY DATA OBJ", data);
-
- function getData(id){
-    fetch('http://localhost:8082/artist/read/'+id)
-      .then(
-        function(response) {
-          if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
-          // Examine the text in the response
-          response.json().then(function(data) {
-             
-           
-            console.log("MY DATA OBJ",data)
-            artistId = id
-             document.querySelector("input#name").value = data.name
-             
-            
-             
-            
-             
-             
-             
-            
-             
-    
-          });
-        }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error :-S', err);
+        artistId = id;
+        document.querySelector("input#name").value = data.name;
       });
     })
     .catch(function (err) {
@@ -62,22 +32,18 @@ document
   .addEventListener("submit", function (stop) {
     stop.preventDefault();
     let formElements = document.querySelector("form.viewRecord").elements;
-    console.log(formElements)
-    
-    
-    let name=formElements["name"].value;
-    console.log("name",name)
-    
+    console.log(formElements);
 
     let name = formElements["name"].value;
+    console.log("name", name);
 
-    sendData(data,artistId)
-   
-  });
+    let data = {
+      name: name,
+    };
 
     console.log("Data to post", data);
 
-    sendData(data, artistid);
+    sendData(data, artistId);
   });
 
 function sendData(data, id) {
